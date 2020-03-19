d1303b5251e689beca0e6e03ab6c84360cbfe085
Randee Orion <randeegood@gmail.com>
2020-03-18 18:31:59 -0700

    some stuff

---
+++ __test__/notes.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Input = require('..lib/input.js');
+
+const badInputA = [];
+const badInputB = ['WRONG'];
+const badInputC = ['-b', 'WRONG'];
+const badInputD = ['-a', ''];
+
+const goodInput = ['-a', 'Notes here.'];
+
+describe('Handles bad input with grace and generosity. Nothing is fatal.', () => {
+  it ('handles empty input', () => {
+    let result = new Input(badInputA);
+    expect(result.valid()).toBeFalsy();
+  });
+  it ('handles wrong input', () => {
+    let result = new Input(badInputb);
+    expect(result.valid()).toBeFalsy();
+  });
+  it ('handles wrong flag', () => {
+    let result = new Input(badInputC);
+    expect(result.valid()).toBeFalsy();
+  });
+  it ('handles wrong data type', () => {
+    let result = new Input(badInputD);
+    expect(result.valid()).toBeFalsy();
+  });
+})
+
+describe('the module handles good input with grace and beauty and without gloating', () => {
+  it ('handles good input for -a flag', () => {
+    let results = new Input(goodInput);
+    expect(result.valid()).toBeTruthy();
+  });
+})
